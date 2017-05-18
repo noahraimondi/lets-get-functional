195d5b235f226b5ad4fc5e6f7682ac814f6a10c5
Olivia Sabo-Rush <liv@Olivias-MacBook.local>
2017-05-18 15:56:13 -0500

    add tests update readme

--- README.md
+++ README.md
@@ -5,20 +5,20 @@ An exercise in problem solving in the functional idiom.
 ## Setup
 
 0. If you haven't release your lodown library to `npm` (node package manager), follow the steps in this video to do so:
-    
+
     [publishing-npm-packages](https://docs.npmjs.com/getting-started/publishing-npm-packages)
-    
+
 
 1. **FORK** this repository on GitHub to your own GitHub account.
 
 2. From your FORK, use SSH to clone the repository into a Blank Cloud9 workspace.
 
 3. Once the Cloud9 workspace is finished initializing, from the bash terminal, run the command:
-    
+
         npm install
 
 4. Install your lodown library by running the command and replacing `<my-user-name>` :
-    
+
         npm install --save lodown-<my-user-name>
 
 5. Open up `index.js` and import your lodown library using the node `require()` method.
@@ -29,14 +29,15 @@ Code and test your solutions in `index.js`. Customer data is available to you in
 
 1. number of males.
 2. number of females
-2. oldest customer, youngest customer
-3. average balance
-4. how many customer's names begin with some letter
-5. how many customer's friend's names begin with some letter
-6. how many customers are friends
-7. users have tags associated with them: find the top 3 most common tags
-8. create a summary of genders, the output should be:
-    
+3. oldest customer
+4. youngest customer
+5. average balance
+6. how many customer's names begin with some letter
+7. how many customer's friend's names begin with some letter
+8. how many customers are friends
+9. users have tags associated with them: find the top 3 most common tags
+10. create a summary of genders, the output should be:
+
 ```javascript
 {
     male: 3,
--- index.js
+++ index.js
@@ -6,31 +6,12 @@ const customers = require("./data/customers.json");
 const _ = require("phrush-lodown");
 
 /**
- * 1. Import your lodown module using the require() method, 
- *    using the string 'lodown-<my-username>', or whatever 
+ * 1. Import your lodown module using the require() method,
+ *    using the string 'lodown-<my-username>', or whatever
  *    name with which you published your npm lodown project.
- * 
+ *
  * 2. Solve all problems as outlined in the README.
  */
- 
-// const maleCount = function(array) {
-//     // Array is an array of objects
-//     // var count = 0;
-//     // what am I working with? An array of objects
-//     // Loop through the arrays
-//     // Loop through object
-//     // Check value of gender property
-//     // IF Male add +1 to 
-//     const memo = _.reduce(array, function(count, array) {
-//         // memo = function()
-//         // console.log(memo);
-//         return count += 1;
-//         console.log(array)
-//         // if(array.gender === "male") {memo.count += 1;} 
-//         // console.log(memo.count)
-//     }, 0);
-//     // return memo.count;
-// }
 
 const maleCount = function(array) {
     return _.reduce(array, function(sum, array) {
@@ -46,7 +27,7 @@ const femaleCount = function(array) {
     }, 0);
 };
 
-const old = function(array) {
+const oldest = function(array) {
     var name = "";
     _.reduce(array, function(sum, array) {
         if(array.age > sum) {
@@ -59,7 +40,7 @@ const old = function(array) {
     return name;
 };
 
-const young = function(array) {
+const youngest = function(array) {
     var name = "";
     _.reduce(array, function(sum, array) {
         if(array.age < sum) {
@@ -72,16 +53,16 @@ const young = function(array) {
     return name;
 };
 
-const youngAndOld = function() {
-    return old(customers) + " is oldest and " + young(customers) + " is youngest.";
-};
+// const youngAndOld = function() {
+//     return old(customers) + " is oldest and " + young(customers) + " is youngest.";
+// };
 
-const avgBal = function(array) {
+const averageBalance = function(array) {
     return _.reduce(array, function(sum, array) {
         sum = sum + parseFloat(array.balance.replace(/[$,]/g,"")); return sum;}, 0) / array.length;
 };
 
-const firstLetter = function(array, letter) {
+const firstLetterCount = function(array, letter) {
     return _.reduce(array, function(count, array) {
         if(array.name.charAt(0).toLowerCase() == letter.toLowerCase()) {
             count += 1;
@@ -91,94 +72,90 @@ const firstLetter = function(array, letter) {
     }, 0);
 };
 
-const friendFirstLetter = function(array, name, letter) {
+const friendFirstLetterCount = function(array, name, letter) {
     return _.reduce(array, function(count, array) {
         if(array.name === name) {
             for(var i = 0; i < array.friends.length; i++) {
                 if(array.friends[i].name.charAt(0).toLowerCase() == letter.toLowerCase()) count += 1;
-            }   
+            }
             return count;
         }
         return count;
     }, 0);
 };
 
-const isFriend = function(array, name) {
-    // pluck and unique
-    // find the properties of all the friends objects
-    // put them in an array
-    // get rid of non-unique names
-    // return
-    
+const friendsCount = function(array, name) {
     let friendsArr = [];
     let customersArr = [];
     let finalArr = [];
-    
     for(var k = 0; k < array.length; k++) {
         for(var j = 0; j < array[k].friends.length; j++) {
             customersArr.push(array[k].name)
             friendsArr.push(array[k].friends[j].name)
         }
     }
-    
     for(var j = 0; j < customersArr.length; j++) {
         if(_.contains(friendsArr, customersArr[j])) { finalArr.push(customersArr[j]) }
     }
-
     return _.unique(finalArr);
 }
 
-const tags = function(array) {
+const topThreeTags = function(array) {
     let tags = _.reduce(array, function(list, array) {
         for(var i = 0; i < array.tags.length; i++) {
             if(array.tags[i] in list) { ++list[array.tags[i]] }
             else { list[array.tags[i]] = 1 }
-        } 
+        }
         return list;
     }, {})
-
     let arr = [];
     let int = 0;
-    
     for(var key in tags) {
         if(tags[key] >= int) {
             arr.unshift(key);
             int = tags[key]
         }
     }
-    
     return _.first(arr, 3)
-    
-    // var tags = _.reduce(array, function(list, array) {
-    //     for(var i = 0; i < array.tags.length; i++) {
-    //         // console.log(array.tags)
-    //         list.push(array.tags[i])
-    //         // console.log(list) 
-    //         // return list;
-    //     }
-    //     return list;
-    //     }, [])
-    //     return tags.sort()
 }
 
-const gender = function(array) {
+const genderCount = function(array) {
     return _.reduce(array, function(object, array) {
-        if(array.gender in object) { ++object[array.gender] } 
+        if(array.gender in object) { ++object[array.gender] }
         else { object[array.gender] = 1; }
         return object;
     }, {})
 }
-    
+
 console.log("Hey! There are",customers.length,"customers!\n");
 console.log("\nMale count is",maleCount(customers));
 console.log("\nFemale count is",femaleCount(customers));
-console.log("\nOldest person is",old(customers));
-console.log("\nYoungest person is",young(customers));
-console.log("\n" + youngAndOld());
-console.log("\nAverage balance is $"+avgBal(customers));
-console.log("\n"+firstLetter(customers,"b"),"customer(s) with a name starting with B.");
-console.log("\n"+firstLetter(customers,"B"),"customer(s) with a name starting with B.");
-console.log("\n"+friendFirstLetter(customers,"Doris Smith","F"),"friend(s) with a name starting with F.");
-console.log("\n",isFriend(customers));
-console.log("\nThe 3 most common tags are",tags(customers));
-console.log("\n",gender(customers));
+console.log("\nOldest person is",oldest(customers));
+console.log("\nYoungest person is",youngest(customers));
+// console.log("\n" + youngAndOld());
+console.log("\nAverage balance is $"+averageBalance(customers));
+console.log("\n"+firstLetterCount(customers,"d"),"customer(s) with a name starting with B.");
+console.log("\n"+firstLetterCount(customers,"a"),"customer(s) with a name starting with B.");
+console.log("\n"+friendFirstLetterCount(customers,"Doris Smith","F"),"friend(s) with a name starting with F.");
+console.log("\n",friendsCount(customers));
+console.log("\nThe 3 most common tags are",topThreeTags(customers));
+console.log("\n",genderCount(customers));
+
+//////////////////////////////////////////////////////////////////////
+// DON'T REMOVE THIS CODE ////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+if((typeof process !== 'undefined') &&
+   (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports.maleCount = maleCount;
+    module.exports.femaleCount = femaleCount;
+    module.exports.oldest = oldest;
+    module.exports.youngest = youngest;
+    module.exports.averageBalance = averageBalance;
+    module.exports.firstLetterCount = firstLetterCount;
+    module.exports.friendFirstLetterCount = friendFirstLetterCount;
+    module.exports.friendsCount = friendsCount;
+    module.exports.topThreeTags = topThreeTags;
+    module.exports.genderCount = genderCount;
+}
