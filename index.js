5e98b915e4b4b960b4856b66eb9d98507cc4412f
Olivia Sabo-Rush <w.phrush@gmail.com>
2017-11-13 19:15:33 -0600

    refactor for installable version

--- index.js
+++ index.js
@@ -1,15 +1,56 @@
-#!/usr/bin/env node
+// #!/usr/bin/env node
 
 'use strict';
 
-const customers = require("./data/customers.json");
-
+const customers = require('./data/customers.json');
+const _ = require('');
 
 /**
- * 1. Import your lodown module using the require() method, 
- *    using the string 'lodown-<my-username>', or whatever 
+ * 1. Import your lodown module using the require() method,
+ *    using the string 'lodown-<my-username>', or whatever
  *    name with which you published your npm lodown project.
- * 
+ *
  * 2. Solve all problems as outlined in the README.
+ *
+ * 3. We started the first one for you as an example! Make the rest in this style:
+ * 
+ * 4. To test your work, run `npm start --prefix ./projects/lets-get-functional` in your terminal.
  */
- 
+
+var maleCount = function(array) {
+
+};
+
+var femaleCount;
+
+var oldestCustomer;
+
+var youngestCustomer;
+
+var averageBalance;
+
+var firstLetterCount;
+
+var friendFirstLetterCount;
+
+var friendsCount;
+
+var topThreeTags;
+
+var genderCount;
+
+//////////////////////////////////////////////////////////////////////
+// DON'T REMOVE THIS CODE ////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+// here, export any references you need for tests //
+module.exports.maleCount = maleCount;
+module.exports.femaleCount = femaleCount;
+module.exports.oldestCustomer = oldestCustomer;
+module.exports.youngestCustomer = youngestCustomer;
+module.exports.averageBalance = averageBalance;
+module.exports.firstLetterCount = firstLetterCount;
+module.exports.friendFirstLetterCount = friendFirstLetterCount;
+module.exports.friendsCount = friendsCount;
+module.exports.topThreeTags = topThreeTags;
+module.exports.genderCount = genderCount;
